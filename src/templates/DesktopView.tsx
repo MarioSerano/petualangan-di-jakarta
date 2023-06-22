49cb65b17d31a3940dacdba7dc68a765622ad489
Mario Serano <marioserano55@gmail.com>
2023-06-22 12:20:15 +0700

    add animation for desktop

--- src/templates/DesktopView.tsx
+++ src/templates/DesktopView.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import "twin.macro";
 
 import AvatarFrame from "@/components/AvatarFrame";
@@ -13,48 +14,76 @@ const DesktopView: FC = (props) => {
       <div tw="relative min-w-[64rem] min-h-[48rem]">
         {/* MS: Absolute Assets */}
         <div tw="w-full h-full absolute top-0 left-0">
-          <div tw="absolute top-0 left-[7.125rem]">
+          <motion.div
+            tw="absolute top-0 left-[7.125rem]"
+            initial={{ opacity: 0, x: "-120px" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, origin: 1 }}
+          >
             <Image
               alt="Left Cloud"
               src="/assets/awan-left.webp"
               width={387}
               height={265}
             />
-          </div>
-          <div tw="absolute top-[5.0625rem] right-8">
+          </motion.div>
+          <motion.div
+            tw="absolute top-[5.0625rem] right-8"
+            initial={{ opacity: 0, x: "120px" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, origin: 1 }}
+          >
             <Image
               alt="Right Cloud"
               src="/assets/awan-right.webp"
               width={387}
               height={309}
             />
-          </div>
+          </motion.div>
         </div>
         {/* Relative assets */}
         <div tw="pt-[5.5625rem] relative flex flex-col justify-center items-center">
-          <div>
+          <motion.div
+            initial={{ opacity: 0.5, y: "200px" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, origin: 1 }}
+          >
             <Image
               alt="Monas"
               src="/assets/monas.webp"
               width={406}
               height={450}
             />
-          </div>
-          <div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
             <Image
               alt="Logo"
               src="/assets/logo.webp"
               width={500}
               height={180}
             />
-          </div>
-          <h1 tw="font-bold text-orange text-[2.5rem] leading-[2.3125rem] mt-6 uppercase">
+          </motion.div>
+          <motion.h1
+            tw="font-bold text-orange text-[2.5rem] leading-[2.3125rem] mt-6 uppercase"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1.5 }}
+          >
             &quot;Petualangan di Jakarta&quot;
-          </h1>
+          </motion.h1>
         </div>
         {/* Character (Absolute) Assets */}
         <div tw="w-full h-full absolute top-0 left-0">
-          <div tw="absolute bottom-[5.8125rem] left-[1.375rem]">
+          <motion.div
+            tw="absolute bottom-[5.8125rem] left-[1.375rem]"
+            initial={{ rotate: 360, left: "-70vh" }}
+            animate={{ rotate: 0, left: 0 }}
+            transition={{ duration: 1 }}
+          >
             <AvatarFrame
               frame={LARGEST_FRAME}
               imageWidth={177}
@@ -63,8 +92,13 @@ const DesktopView: FC = (props) => {
               alt="Luna"
               color={characterColors.luna}
             />
-          </div>
-          <div tw="absolute top-[10.3125rem] left-[8.3125rem]">
+          </motion.div>
+          <motion.div
+            tw="absolute top-[10.3125rem] left-[8.3125rem]"
+            initial={{ rotate: -360, left: "-80vh" }}
+            animate={{ rotate: 0, left: 0 }}
+            transition={{ duration: 1 }}
+          >
             <AvatarFrame
               frame={SMALLEST_FRAME}
               imageWidth={150}
@@ -73,8 +107,13 @@ const DesktopView: FC = (props) => {
               alt="Indah"
               color={characterColors.indah}
             />
-          </div>
-          <div tw="absolute bottom-[13.625rem] right-[1.375rem]">
+          </motion.div>
+          <motion.div
+            tw="absolute bottom-[13.625rem] right-[1.375rem]"
+            initial={{ rotate: 360, right: "-70vh" }}
+            animate={{ rotate: 0, right: 0 }}
+            transition={{ duration: 1 }}
+          >
             <AvatarFrame
               frame={LARGEST_FRAME}
               imageWidth={192}
@@ -83,7 +122,7 @@ const DesktopView: FC = (props) => {
               alt="Rani"
               color={characterColors.rani}
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
